08b5ac7b633107bbb7ef20b1d4d6a9278acd7d00
kevin andres calle marin <k.calle@utp.edu.co>
2022-08-20 16:21:06 -0500

    back with jwt

--- back/database/migrations/2014_10_12_000000_create_users_table.php
+++ back/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('id_rol');
             $table->rememberToken();
             $table->timestamps();
         });
