8eaa56534252fd4dbb5994831aa002effb08e338
magnum2334 <kevincallem19@gmail.com>
2023-01-30 03:11:28 -0600

    sms okey

--- back/routes/api.php
+++ back/routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\ContactUsController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
-    //Route::post('register', 'register');
+    Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
     Route::get('me', 'me');
