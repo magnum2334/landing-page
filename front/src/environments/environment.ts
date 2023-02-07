c1168b0e1e42176a5a4121aaffd4835f54e67762
magnum2334 <kevincallem19@gmail.com>
2023-02-07 11:24:56 -0600

    nuevos campos para contacts

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -10,8 +10,8 @@
 
 export const environment = {
   baseUrl: "http://localhost:8000/api/",
-  storageUrl: "http://localhost:8000/api/",
-  production: true
+  storageUrl: "http://localhost:8000/storage/",
+  production: false
 };
 
 /*
